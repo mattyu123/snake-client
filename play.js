84859d37d324a54106346889da2ba2ba274a91d5
mattyu123 <matthewyu100@gmail.com>
2023-05-19 15:41:13 -0400

    move connect function to separate module

--- play.js
+++ play.js
@@ -1,13 +1,6 @@
+const connect = require("./client.js")
+
 const net = require("net");
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 3000
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-}
+console.log("Connecting ...");
+connect();
