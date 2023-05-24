da7e8efe379892af4cbff591807a7d60ab4ded3b
mattyu123 <matthewyu100@gmail.com>
2023-05-24 08:29:21 -0400

    returns users name when the connection to the server has been made

--- client.js
+++ client.js
@@ -10,22 +10,31 @@ const connect = function() {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
+  })
+
+  //Lets the client side know that they have successfully connected to the server 
+  conn.on("connect", () => {
     console.log("A successful connection has been made to the server")
   })
 
+  //Name for the snake is shown on the server and moves the snake up one spot on the board
   conn.on("connect", () => {
     conn.write("Name: M.Y");
+    // conn.write("Move: up");
   });
+
+  //Name for the snake is shown on the server and moves the snake up one spot on the board
+  // conn.on("connect", () => {
+  //   setTimeout(() => conn.write("Move: left"), 50); 
+  //   });
 
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
 
-  conn.on("connect", () => {
-  })
-
   return conn;
 }
+
 
 module.exports = {
   connect,
